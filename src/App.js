d570884c3c22c9a34334e6c337e04a2fb1501cbf
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-06-19 15:05:34 -0400

    Created wolves.json file. Created map function for Wolf Component properties. Addedstyling to WolfCard component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import Title from "./Components/Title"
-import Wrapper from "./Components/Wrapper"
+import Title from "./Components/Title";
+import Wrapper from "./Components/Wrapper";
+import WolfCard from './Components/WolfCard';
+import wolves from "./wolves.json";
 
 class App extends Component {
+
+  state = {
+    wolves
+  };
 
   render(){
     return(
       <Wrapper>
         <Title>The Wolf Page</Title>
-
+        {this.state.wolves.map(wolve => (
+          <WolfCard
+          name={wolve.name}
+          animaltype={wolve.animaltype}
+          
+          />
+        ))}
+        
       </Wrapper>
       )
   }
---
+++ src/Components/WolfCard/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+function WolfCard(props){
+    return(
+        <div className="card">
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name: </strong> {props.name}
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <strong>Animal Type: </strong> {props.animaltype}
+                    </li>
+                </ul>
+            </div>
+    
+        </div>
+    )
+    
+}
+
+export default WolfCard;
