ed1680e42d33f4e2972c35c488103c520e83e753
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-07-22 15:38:37 -0400

    Updated About Component paragraph

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -3,7 +3,12 @@ import "./style.css";
 
 function About(){
     return(
-        <p className="paraone">The wolf is my favorite animal. I like it because it's beautifal, its behaviors such as traveling in a pack, and of course the notable howl the wolf is known for.</p>
+        <p className="paraone">The wolf is my favorite animal. 
+        Why is it my favorite animal? For a few reasons. First and foremost, it
+        has a beautifal and majestic look that makes it immediately recognizable. Another reason
+        is their unmistakable howl. In wilderness movies, when you hear a howl, you know there a wolves around.
+        Finally, it's their connection to family. The wolf is known for traveling in a pack.  
+         </p>
 
 
     );
