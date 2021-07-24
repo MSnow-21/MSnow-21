65cbfea5e6abbf02262a602853c1b429fc4d4cda
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-07-24 08:35:06 -0400

    Updated inputs for wolves.json. Added more WolfCard components to App.js. Updated styling on WolfCard css.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import WolfCard from './Components/WolfCard';
 import About from './Components/About';
 import wolves from "./wolves.json";
 import wolfimageone from "./Images/andrew-ly-iUA1cea8QiY-unsplash.jpg";
-import wolfimagefour from "./Images/chris-ensminger-i6yrDhfZ-XQ-unsplash.jpg";
+import wolfimagetwo from "./Images/chris-ensminger-i6yrDhfZ-XQ-unsplash.jpg";
 import wolfimagethree from "./Images/marek-szturc-CM1oVEUzsNM-unsplash.jpg";
+import wolfimagefour from "./Images/tahoe-Ikge1ZIk9yc-unsplash.jpg";
 
 
 function App(){
@@ -15,18 +16,47 @@ function App(){
       <Title>The Wonderful Wolf</Title>
       <About />
       <WolfCard
-      number={wolves[0].id}
+      number={wolves[0].number}
       fact={wolves[0].fact}
       image={wolfimageone}
       />
       <WolfCard
+      number={wolves[1].number}
       fact={wolves[1].fact}
-      image={wolfimagefour}
+      image={wolfimagetwo}
       />
       <WolfCard
+      number={wolves[2].number}
       fact={wolves[2].fact}
       image={wolfimagethree}
       />
+      <WolfCard
+      number={wolves[3].number}
+      fact={wolves[3].fact}
+      image={wolfimagefour}
+      />
+            <WolfCard
+      number={wolves[3].number}
+      fact={wolves[3].fact}
+      image={wolfimagefour}
+      />
+                  <WolfCard
+      number={wolves[3].number}
+      fact={wolves[3].fact}
+      image={wolfimagefour}
+      />
+                        <WolfCard
+      number={wolves[3].number}
+      fact={wolves[3].fact}
+      image={wolfimagefour}
+      />
+                        <WolfCard
+      number={wolves[3].number}
+      fact={wolves[3].fact}
+      image={wolfimagefour}
+      />
+
+      
     </Wrapper>
     );
 
--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -6,8 +6,8 @@ function About(){
         <p className="paraone">The wolf is my favorite animal. 
         Why is it my favorite animal? For a few reasons. First and foremost, it
         has a beautifal and majestic look that makes it immediately recognizable. Another reason
-        is their unmistakable howl. In wilderness movies, when you hear a howl, you know there a wolves around.
-        Finally, it's their connection to family. The wolf is known for traveling in a pack.  
+        is their unmistakable howl. In wilderness movies, when you hear a howl, you know there are wolves around.
+        Finally, it's their connection to family. The wolf is known for its bond with a pack.  
          </p>
 
 
